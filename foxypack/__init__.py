58c857940ad6ff05e1e535ae1e696fbfc14d7488
Dima Shapranov <shapranov.work@gmail.com>
2025-10-03 14:53:57 +0300

    add __all__ dependencies

---
+++ foxypack/__init__.py
@@ -0,0 +1,7 @@
+from foxypack.abc.foxyanalysis import FoxyAnalysis
+from foxypack.abc.foxystat import FoxyStat
+from foxypack.answers import AnswersAnalysis, AnswersStatistics
+from foxypack.controller import FoxyPack
+
+
+__all__ = [FoxyAnalysis, FoxyStat, FoxyPack, AnswersAnalysis, AnswersStatistics]
