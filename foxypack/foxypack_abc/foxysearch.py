57c159f97e0133ae601a7135a6cc0638f098ccda
Dima Shapranov <shapranov.work@gmail.com>
2025-11-20 14:06:09 +0300

    fixation

--- foxypack/foxypack_abc/foxysearch.py
+++ foxypack/foxypack_abc/foxysearch.py
@@ -1,5 +1,16 @@
 from abc import ABC
+from typing import Generator
+
+from foxypack import FoxyStat
+
+
+class EntitiesFoundGenerator(Generator):
+    pass
 
 
 class FoxySearch(ABC):
-    pass
+    def __init__(self, statistics_engine: FoxyStat) -> None:
+        self.statistics_engine = statistics_engine
+
+    def get_search_fox(self):
+        pass
