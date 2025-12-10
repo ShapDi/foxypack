3e6bb6c0c8824a7df411fb217d292e1ae92ea3cc
Dima Shapranov <shapranov.work@gmail.com>
2025-12-10 14:19:11 +0300

    del entitys

--- foxypack/__init__.py
+++ foxypack/__init__.py
@@ -2,9 +2,6 @@ from foxypack.foxypack_abc.foxyanalysis import FoxyAnalysis
 from foxypack.foxypack_abc.foxystat import FoxyStat
 from foxypack.foxypack_abc.answers import AnswersAnalysis, AnswersStatistics
 from foxypack.controller import FoxyPack
-from foxypack.entitys.balancers import BaseEntityBalancer, Entity
-from foxypack.entitys.pool import EntityPool
-from foxypack.entitys.storage import Storage
 from foxypack.exceptions import DenialAsynchronousService, DenialSychronService
 
 
@@ -14,10 +11,6 @@ __all__ = [
     "FoxyPack",
     "AnswersAnalysis",
     "AnswersStatistics",
-    "BaseEntityBalancer",
-    "Entity",
-    "EntityPool",
-    "Storage",
     "DenialSychronService",
     "DenialAsynchronousService",
 ]
